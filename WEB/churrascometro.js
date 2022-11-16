fb3db8bb6d19fed7f0162b3d083bf9c1119d76f6
Icaro Siqueira Pinto <70227039+icaroweb0@users.noreply.github.com>
2022-11-16 05:12:48 -0300

    Create churrascometro.js

---
+++ WEB/churrascometro.js
@@ -0,0 +1,5 @@
+//var mediaCarne, totalCarne, precoCarne, precoTotal, valorPorPessoa, qtdePessoas
+
+console.log("Insira a quantidade de carne que uma pessoa consome em média. \n")
+
+let pergunta = prompt("teste")
